8f9b803c9cca862190a839e6b880dffa56c0b5da
ezekielnelson060-hash <ezekielnelson060@gmail.com>
2026-08-02 15:43:18 +0100

    feat: mount RosterProvider for multi-artist context

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,13 +1,16 @@
 "use client";
 
 import { PlanProvider } from "@/components/billing/plan-provider";
+import { RosterProvider } from "@/lib/roster-context";
 import { CookieConsent } from "@/components/cookie-consent";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <PlanProvider>
-      {children}
-      <CookieConsent />
+      <RosterProvider>
+        {children}
+        <CookieConsent />
+      </RosterProvider>
     </PlanProvider>
   );
 }
